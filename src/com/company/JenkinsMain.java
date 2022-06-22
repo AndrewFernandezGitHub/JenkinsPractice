5c5dfb7ecb960736a7bb4ec59b1c5522093b8238
AndrewFernandeGitHub <drew12f@gmail.com>
2022-06-21 21:43:03 -0400

    2nd jenkins commit

--- src/com/company/JenkinsMain.java
+++ src/com/company/JenkinsMain.java
@@ -4,6 +4,7 @@ public class JenkinsMain {
     public static void main(String[] args) {
         for(int i=0;i<5;i++){
             System.out.println("Hello, I am andrew");
+            System.out.println("Hello, I am New Andrew");
         }
     }
 }
